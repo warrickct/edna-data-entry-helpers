7325b6f2cc1487c2b8b591a94a2a04ac53607db8
warrick <wcor690@aucklanduni.ac.nz>
2018-07-25 11:38:12 +1200

    Fixed shortening of row by removing subtraction from range

--- removeduplicates.py
+++ removeduplicates.py
@@ -1,45 +1,44 @@
 import csv
 import glob
 
+def make_writable_line(list):
+            return '\t'.join(str(value) for value in list) + '\n'
+
+def sum_row_values(r, r2):
+    print('r: ', len(r))
+    print('r2: ', len(r2))
+    new_row = []
+    new_row.append(r[0])
+    for index in range(1, len(r)):
+        total = float(r[index]) + float(r2[index])
+        new_row.append(total)
+    return new_row
+
 files = set(glob.glob('./test_inputs/new-data/*.tsv')) - set(glob.glob('./test_inputs/new-data/*removed_duplicates.tsv'))
 for fname in files:
     with open(fname, 'rU') as input_file:
-        output_file = open('%s-removed_duplicates.tsv' % fname, "w+")
-
-        def make_writable_line(list):
-            return '\t'.join(str(value) for value in list) + '\n'
-
-        def sum_row_values(r, r2):
-            new_row = []
-            new_row.append(r[0])
-            for index in range(1, len(r)-1):
-                total = float(r[index]) + float(r2[index])
-                new_row.append(total)
-            return new_row
-
         input_reader = csv.reader(input_file, delimiter='\t')
         headers = next(input_reader)
-        output_file.write(make_writable_line(headers))
-        taxons_entered = {}
-        total_rows = 0
+        rows_checked = 0
         duplicate_count = 0
-        new_file_object = {}
+        new_rows = {}
+        row_lookup = []
         for index, otu_row in enumerate(input_reader):
-            total_rows += 1
+            rows_checked += 1
             otu_name = otu_row[0]
-            if otu_name in taxons_entered:
-                new_file_object[index] = sum_row_values(taxons_entered[otu_name], otu_row)
+            if otu_name in new_rows:
+                new_rows[otu_name] = sum_row_values(new_rows[otu_name], otu_row)
                 duplicate_count += 1         
             else:
-                new_file_object[index] = otu_row
-                taxons_entered[otu_name] = otu_row
+                new_rows[otu_name] = otu_row
+            row_lookup.append(otu_name)
 
     with open('%s-removed_duplicates.tsv' % fname, "w+") as output_file:
-        output_file.write(make_writable_line(headers))
-        for list in new_file_object:
-            output_file.write(make_writable_line(new_file_object[list]))
+        writer = csv.writer(output_file, delimiter="\t")
+        writer.writerow(headers)
+        # writer.writerows(new_rows)
+        for index in row_lookup:
+            writer.writerow(new_rows[index])
 
-        
     print('remove %d rows' % duplicate_count)
-    print('total %d rows' % total_rows)
-    
+    print('total %d rows' % rows_checked)
