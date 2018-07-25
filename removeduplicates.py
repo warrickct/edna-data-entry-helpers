aed33d5861214e1172351eed54be59b0dcf85974
warrick <wcor690@aucklanduni.ac.nz>
2018-07-25 14:03:52 +1200

    Fixed the repeat writes to file so it doesnt included the duplicates now

--- removeduplicates.py
+++ removeduplicates.py
@@ -1,5 +1,6 @@
 import csv
 import glob
+from collections import OrderedDict
 
 def sum_row_values(r, r2):
     '''
@@ -18,24 +19,22 @@ for fname in files:
         input_reader = csv.reader(input_file, delimiter='\t')
         headers = next(input_reader)
         rows_checked = 0
-        duplicate_count = 0
-        otu_row_dict = {}
-        otu_order = []
+        combined_count = 0
+        otu_row_dict = OrderedDict()
         for otu_key, otu_row in enumerate(input_reader):
             rows_checked += 1
             otu_name = otu_row[0]
             if otu_name in otu_row_dict:
                 otu_row_dict[otu_name] = sum_row_values(otu_row_dict[otu_name], otu_row)
-                duplicate_count += 1         
+                combined_count += 1         
             else:
                 otu_row_dict[otu_name] = otu_row
-            otu_order.append(otu_name)
 
     with open('%s-removed_duplicates.tsv' % fname, "w+") as output_file:
         writer = csv.writer(output_file, delimiter="\t")
         writer.writerow(headers)
-        for otu_key in otu_order:
-            writer.writerow(otu_row_dict[otu_key])
+        for row in otu_row_dict:
+            writer.writerow(otu_row_dict[row])
 
-    print('Rows combined: %d' % duplicate_count)
+    print('Rows combined: %d' % combined_count)
     print('Rows checked: %d' % rows_checked)
